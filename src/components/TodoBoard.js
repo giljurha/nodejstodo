df01ced07c83c50931657aec7a4eaafedd10a889
giljurha <giljurha93@gmail.com>
2025-07-26 17:34:15 +0900

    push

--- src/components/TodoBoard.js
+++ src/components/TodoBoard.js
@@ -1,15 +1,16 @@
 import React from "react";
 import TodoItem from "./TodoItem"; // Assuming you have a TodoItem component
-const TodoBoard = ({todoList}) => {
+const TodoBoard = ({ todoList }) => {
   return (
     <div>
       <h2>Todo List</h2>
       {todoList.length > 0 ? (
-        todoList.map((item) => <TodoItem item={item} />)
-      ) : (     
+        todoList.map((item) => (
+          <TodoItem key={item.id} item={item} />
+        ))
+      ) : (
         <h2>There is no Item to show</h2>
       )}
-      <h2>There is no Item to show</h2>
     </div>
   );
 };
